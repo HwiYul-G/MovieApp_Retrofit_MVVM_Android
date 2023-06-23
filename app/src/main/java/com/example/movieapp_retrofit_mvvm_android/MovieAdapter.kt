19bd75cd74dfe6c43dc1a9c7b2bec68a251c8ee2
Y <hwiyul_g@daum.net>
2023-06-23 19:53:43 +0900

    Add : RecyclerView의 Adapter와 ViewHolder

---
+++ app/src/main/java/com/example/movieapp_retrofit_mvvm_android/MovieAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.movieapp_retrofit_mvvm_android
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.movieapp_retrofit_mvvm_android.databinding.MovieLayoutBinding
+
+class MovieAdapter : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+    // Retrofit으로 받아온 영화 list 정보를 담을 배열
+    private var movieList = ArrayList<Result>()
+    fun setMovieList(movieList : List<Result>){
+        this.movieList = movieList as ArrayList<Result>
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(val binding : MovieLayoutBinding) : RecyclerView.ViewHolder(binding.root)  {}
+
+    override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : ViewHolder {
+        return ViewHolder(
+            MovieLayoutBinding.inflate(
+                LayoutInflater.from(parent.context)
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder : ViewHolder, position : Int) {
+        Glide.with(holder.itemView)
+            .load("https://image.tmdb.org/t/p/w500${movieList[position].poster_path}")
+            .into(holder.binding.movieImage)
+
+        holder.binding.movieName.text = movieList[position].title
+    }
+
+    override fun getItemCount(): Int {
+        return movieList.size
+    }
+}
